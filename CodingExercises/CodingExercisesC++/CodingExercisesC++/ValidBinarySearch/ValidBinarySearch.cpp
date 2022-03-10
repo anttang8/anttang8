0474a0e96fd4e0334c05557ba512851e551f7c7e
Anthony Tang <anttang8@gmail.com>
2022-03-09 22:19:39 -0800

    Initial commit with structure

---
+++ CodingExercises/CodingExercisesC++/CodingExercisesC++/ValidBinarySearch/ValidBinarySearch.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+namespace ValidBinarySearch
+{
+    // 
+    int main()
+    {
+        // Exercise: You are given a binary search and you want to test whether or note
+        // a binary tree is valid or not. A valid binary search tree is a tree to which
+        // all left child nodes of a given node are less than it and all right child
+        // nodes are greater than it.
+        std::cout << "__________________________________________________________\n";
+        std::cout << "Session 2 - Valid Binary Search\n";
+
+
+        std::cout << "__________________________________________________________\n";
+        return 0;
+    }
+}
